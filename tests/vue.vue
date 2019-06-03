7333180c0a2b493a062326246ba80c273aa1dcee
Mao <mao8a87@gmail.com>
2019-06-02 19:51:34 -0500

    vue file for example

---
+++ tests/vue.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>{{header}}</h1>
+      <article>
+        <section>
+          <h3>{{title}}</h3>
+          <p>{{text}}</p>
+          <a :href="link" target="_blank">Link</a>
+        </section>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Component",
+    data(){
+      return{
+        header: "header",
+        title: "Title",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos obcaecati aliquid et, commodi inventore delectus enim laborum voluptates",
+        link: "https://www.google.com/"
+      }
+    },
+    watch:{
+    },
+    methods:{
+      methodA(){
+        console.log("methodA")
+      }
+    },
+    computed:{
+    },
+    mounted(){
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.main-container{
+  max-width: 900px;
+  display: flex;
+  justify-content: center;
+}
+</style>
