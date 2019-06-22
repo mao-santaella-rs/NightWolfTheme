07d756450a956de417f9c1f82bb487114ccdc732
Mao <mao8a87@gmail.com>
2019-06-22 15:56:22 -0500

    starting

---
+++ source/colors/light.js
@@ -0,0 +1,61 @@
+const Color = require("color")
+const colors = require("./common-colors")
+
+// THEME COLORS
+colors.principal =            Color.rgb(240,240,240)
+colors.principal_0 =          Color.rgb(214,213,217)
+colors.principal_1 =          Color.rgb(176,176,184)
+colors.principal_2 =          Color.rgb(139,138,150)
+colors.principal_3 =          Color.rgb(101,101,117)
+colors.principal_4 =          Color.rgb(64,63,83)
+
+colors.text =                 colors.principal_4
+
+colors.selection =            colors.principal_3
+colors.selectionHighlight =   colors.principal_2
+colors.selectionInactive =    colors.selection
+colors.hoverHiglight =        colors.selection
+colors.wordHighlightStrong =  colors.selection
+colors.findMatch =            colors.principal_3
+colors.findHighlight =        colors.selectionHighlight
+colors.rangeHiglight =        colors.principal_1
+colors.lineHiglight =         colors.principal_0
+
+colors.comment =              Color.rgb(100,120,130)
+
+// SYNTAX COLORS
+// General colors
+colors.variable =             colors.syntaxYellow
+colors.variableProperty =     colors.text
+colors.contrastText =         colors.syntaxRed
+colors.variableInstance =     colors.syntaxOrange
+colors.specialWordA =         colors.syntaxBlue
+colors.specialWordB =         colors.syntaxViolet
+colors.specialWordC =         colors.syntaxPurple
+colors.functionName =         colors.syntaxCyan
+colors.string =               colors.syntaxGreen
+colors.operator =             colors.contrastText
+colors.regularText =          colors.text
+colors.number =               colors.syntaxOrange
+colors.boolean =              colors.contrastText
+colors.nullUnd =              colors.contrastText
+colors.punctuation =          colors.regularText
+colors.deprecated =           colors.danger
+
+// Markup
+colors.htmlTag =              colors.contrastText
+colors.metaTag =              colors.text
+colors.tagAttribute =         colors.specialWordB
+colors.pugClass =             colors.variable
+
+// Stylesheets
+colors.cssAttribute =         colors.functionName
+colors.cssValue =             colors.text
+colors.cssTag =               colors.contrastText
+colors.cssClass =             colors.variable
+colors.cssId =                colors.specialWordA
+colors.cssProperty =          colors.functionName
+colors.cssUnits =             colors.number
+colors.cssSpecialWord =       colors.functionName
+
+module.exports = colors
---
+++ source/night-wolf-light.js
@@ -0,0 +1,5 @@
+const colors = require("./colors/light")
+
+const theme = require("./base/dark")
+
+module.exports = theme(colors)
